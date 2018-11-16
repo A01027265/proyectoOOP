b384647e72ab45451b3464634301bcdbba347055
A01027265 <A010127265@itesm.mx>
2018-11-16 16:56:47 -0600

    Finished user class

--- Code/User.java
+++ Code/User.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+
+import java.io.*;
+
 public class User {
 
     private String username;
@@ -5,6 +9,7 @@ public class User {
     private String firstname, lastname;
     private UserType userType;
 
+    // Constructor
     public User(String username, String password, String firstname, String lastname, int userType){
         this.username = username;
         this.password = password;
@@ -13,6 +18,7 @@ public class User {
         decideUserType(userType);
     }
 
+    // Method to decide which user type and assign enum UserType
     public void decideUserType(int userType){
         switch (userType){
             case 1:
@@ -25,6 +31,44 @@ public class User {
                 userType = User.VENDOR;
                 break;
         }
+    }
+
+    // Accessors
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFullname(){
+        return firstname + " " + lastname;
+    }
+
+    // Mutators
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
     }
 
 }
